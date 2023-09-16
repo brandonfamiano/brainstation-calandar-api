8beb9dd7f4e67856538b3a29de79ac782d789c34
brandonfamiano <brandonfamiano@gmail.com>
2023-09-16 13:17:28 -0400

    rendered calendar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay"
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import React{ useState } from "react";
 import DatePicker from 'react-datepicker';
+import { useState } from 'react';
 
 const locales ={
-  'en-US': require('date-fns/locale/en-US');
+  'en-US': require('date-fns/locale/en-US')
 }
 
 const localizer = dateFnsLocalizer({
@@ -23,16 +23,18 @@ const localizer = dateFnsLocalizer({
 })
 
 const events =[
-  title: "event1",
-  allday: true
-  start: new Date(2023, 9, 16)
-  end: new Date(2023, 9, 16)
+  {
+    title: "event1",
+    allday: true,
+    start: new Date(2023, 9, 16),
+    end: new Date(2023, 9, 16)
+  },
 ]
 
 function App() {
   return (
     <div className="body">
-      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end"></Calendar>
+      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style = {{height:500, margin: "50px"}}></Calendar>
     </div>
   );
 }
