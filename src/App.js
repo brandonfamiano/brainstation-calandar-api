7aff23dc8c7b1d76610f00d1b08b5b3046d16464
brandonfamiano <brandonfamiano@gmail.com>
2023-09-16 13:08:46 -0400

    init calendar app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay"
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import React{ useState } from "react";
+import DatePicker from 'react-datepicker';
+
+const locales ={
+  'en-US': require('date-fns/locale/en-US');
+}
+
+const localizer = dateFnsLocalizer({
+
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const events =[
+  title: "event1",
+  allday: true
+  start: new Date(2023, 9, 16)
+  end: new Date(2023, 9, 16)
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end"></Calendar>
     </div>
   );
 }
