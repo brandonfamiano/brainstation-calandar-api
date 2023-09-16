dd9ac750d59ce4c53b0bd25a4f021cfca2bb375d
brandonfamiano <brandonfamiano@gmail.com>
2023-09-16 13:59:44 -0400

    implemented add event logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
@@ -7,7 +7,6 @@ import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay"
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import DatePicker from 'react-datepicker';
-import { useState } from 'react';
 
 const locales ={
   'en-US': require('date-fns/locale/en-US')
@@ -26,15 +25,32 @@ const events =[
   {
     title: "event1",
     allday: true,
-    start: new Date(2023, 9, 16),
-    end: new Date(2023, 9, 16)
+    start: new Date(2023,8, 16),
+    end: new Date(2023,8  , 16)
   },
 ]
 
 function App() {
+  const [newEvent, setNewEvent] = useState({title:"", start:"",end:""})
+  const [allEvents, setAllEvents] = useState(events )
+
+  function handleAddEvent(){
+    setAllEvents([...allEvents, newEvent])
+  }
   return (
     <div className="body">
-      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style = {{height:500, margin: "50px"}}></Calendar>
+      <h1>Calendar</h1>
+      <h2>Add an Event</h2>
+      <div>
+        <input type='text' placeholder='Add Title' style={{width:'20%', marginRight:"10px"}}
+        value={newEvent.title} onChange={(e)=> setNewEvent({...newEvent, title: e.target.value})}/>
+      </div>
+      <DatePicker placeholderText='Start Date' style={{marginRight:'10px'}}
+      selected ={newEvent.start} onChange={(start)=> setNewEvent({...newEvent, start})}/>
+      <DatePicker placeholderText='End Date' 
+      selected ={newEvent.end} onChange={(end)=> setNewEvent({...newEvent, end})}/>
+      <button style = {{margin:'10px'}} onClick={handleAddEvent}>Add Event</button>
+      <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style = {{height:500, margin: "50px"}}></Calendar>
     </div>
   );
 }
